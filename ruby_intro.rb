6c419cfa957b57991b7d8497fa6ee907f2ff74a3
dvp416 <33526821+dvp416@users.noreply.github.com>
2019-09-17 16:11:37 -0500

    starts_with_consonants

---
+++ ruby_intro.rb
@@ -0,0 +1,69 @@
+# When done, submit this entire file to the autograder.
+
+# Part 1
+
+def sum(arr)
+  # YOUR CODE HERE
+  if arr.empty?
+    0
+  else
+    i = 0
+    out = 0
+    while i < arr.length
+      out += arr[i]
+      i += 1
+    end
+    out
+  end
+end
+
+def max_2_sum(arr)
+  # YOUR CODE HERE
+  if arr.empty?
+    0
+  elsif arr.length == 1
+    arr[0]
+  else
+    maxes = arr.max(2)
+    maxes[0] + maxes[1]
+  end
+end
+
+def sum_to_n?(arr, n)
+  # YOUR CODE HERE
+  if arr.empty?
+    false
+  elsif arr.length == 1
+    false
+  else
+    arr.combination(2).any?{ |first, second| first + second == n}
+  end
+end
+
+# Part 2
+
+def hello(name)
+  # YOUR CODE HERE
+  'Hello, ' + name
+end
+
+def starts_with_consonant?(s)
+  # YOUR CODE HERE
+  if s.empty?
+    false
+  elsif (s !~ /\A[aeiouAEIOU!@#$%^&*()]/) && (s =~ /[\w]*/)
+    true
+  else
+    false
+  end
+end
+
+def binary_multiple_of_4? s
+  # YOUR CODE HERE
+end
+
+# Part 3
+
+class BookInStock
+# YOUR CODE HERE
+end
